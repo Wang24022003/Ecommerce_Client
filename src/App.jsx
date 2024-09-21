869cc031be493cf6f651f76f4f4e6e0e6a0e14b9
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-09-21 23:52:01 +0700

    Shop Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Shops from './pages/Shops';
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
-
+      <Route path='/shops' element={<Shops/>} />
+      
     </Routes>
     
     </BrowserRouter>
