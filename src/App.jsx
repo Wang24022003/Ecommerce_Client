dd50a41d85ffcfe676dc8e8e039ce76d8d5a932b
Wang24022003 <113271689+Wang24022003@users.noreply.github.com>
2024-09-24 09:29:06 +0700

    category and Product Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-// import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
@@ -6,16 +7,29 @@ import Shops from './pages/Shops';
 import Card from './pages/Card';
 import Shipping from './pages/Shipping';
 import Details from './pages/Details';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { get_category } from './store/reducers/homeReducer';
+import { useDispatch} from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(get_category()) 
+},[])
+
+
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
       <Route path='/shops' element={<Shops/>} />
       <Route path='/card' element={<Card/>} />
       <Route path='/shipping' element={<Shipping/>} />
       <Route path='/product/details/:slug' element={<Details/>} /> 
+
     </Routes>
     
     </BrowserRouter>
